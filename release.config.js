54981cadc7cffc567287d39fdd4b4dcb3c9ad6c9
Jonathan Greenemeier <jonathan.greenemeier31@t-mobile.com>
2021-01-10 16:42:54 -0700

    fix: update release config

--- release.config.js
+++ release.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '@semantic-release/npm',
     {
       path: '@semantic-release/git',
-      assets: ['dist/**/*.{js,css}', 'docs/*', 'package.json', 'CHANGELOG.md'],
+      assets: ['dist/**/*.{js,css}', 'docs', 'package.json', 'CHANGELOG.md'],
       message:
         'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
     },
